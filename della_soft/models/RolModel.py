f04a516ffb38f83d5825eb3278535a9c719de2e2
David Bareiro <davidbareiro11@gmail.com>
2025-03-19 21:50:37 -0300

    Database corrections (FK relationships). SQL Model Metadata Create All in ConnectDB Class.

--- della_soft/models/RolModel.py
+++ della_soft/models/RolModel.py
@@ -1,14 +1,17 @@
 import reflex as rx
 
-from typing import Optional
-from sqlmodel import Field
+from typing import List, Optional, TYPE_CHECKING
+from sqlmodel import Field, Relationship
 
-
-from rxconfig import config
+if TYPE_CHECKING:
+    from .CustomerModel import Customer
 
 class Rol (rx.Model, table=True):
+    __tablename__ = "rol"
+
     id_rol: Optional[int] = Field(default=None, primary_key=True)
     description: str
 
-'''engine = create_engine(config.db_url, echo=True)
-SQLModel.metadata.create_all(engine)'''
+    customers: List["Customer"] = Relationship(
+        back_populates="rol"
+    )
