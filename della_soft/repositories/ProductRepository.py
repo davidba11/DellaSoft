a20103b631df2f37b67862b8dd42a84706d7a9f2
David Bareiro <davidbareiro11@gmail.com>
2025-03-25 17:50:16 -0300

    Product Modal add, Search products and models creation

--- della_soft/repositories/ProductRepository.py
+++ della_soft/repositories/ProductRepository.py
@@ -1,10 +1,35 @@
 from ..models.ProductModel import Product
 from .ConnectDB import connect
-from sqlmodel import Session, select
+from sqlmodel import Session, or_, select
 
 
 def select_all():
     engine = connect()
     with Session(engine) as session:
         query = select(Product)
+        return session.exec(query).all()
+    
+def insert_product(product: Product):
+    engine = connect()
+    with Session(engine) as session:
+        session.add(product)
+        session.commit()
+        query = select(Product)
+        return session.exec(query).all()
+    
+def get_product(value: str):
+    engine = connect()
+    with Session(engine) as session:
+        query = select(Product).where(
+            or_(
+                Product.name.ilike(f"%{value}%"),  # Busca coincidencias parciales en nombre
+                Product.description.ilike(f"%{value}%"),   # Busca coincidencias parciales descripción
+                Product.product_type.ilike(f"%{value}%"),
+            ))
+        return session.exec(query).all()
+    
+def get_by_id(id: int):
+    engine = connect()
+    with Session(engine) as session:
+        query = select(Product).where(Product.id == id)
         return session.exec(query).all()
--- della_soft/services/ProductService.py
+++ della_soft/services/ProductService.py
@@ -1,6 +1,17 @@
-from ..repositories.ProductRepository import select_all
+from ..repositories.ProductRepository import select_all, get_by_id, get_product, insert_product
+from ..models.ProductModel import Product
 
 def select_all_product_service():
     products = select_all()
-    print (products)
     return products
+
+def select_product(value: str):
+    if(len(value) != 0):
+        return get_product(value)
+    else:
+        return select_all()
+
+def create_product(id: int, name: str, description: str, product_type: str):
+
+    product_save = Product(id=id, name=name, description=description, product_type=product_type)
+    return insert_product(product_save)
